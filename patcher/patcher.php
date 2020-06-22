eeb52a91f7b9eb2ce5845105f6b7c4ee8efddbb8
Franco Catrin <fcatrin@gmail.com>
2020-06-22 14:24:57 -0400

    Add search/replace patches support

--- patcher/patcher.php
+++ patcher/patcher.php
@@ -14,15 +14,24 @@ if ($argc < 3) {
 
 $xexfile     = $argv[1];
 $xexpatch    = $argv[2];
-$patchedfile = $argc < 4 ? "patched_$xexfile" : $argv[3];
 
+if ($argc > 3) {
+	$patchedfile = $argv[3];
+} else {
+	$srcfile = realpath($xexfile);
+	$dir = dirname($srcfile);
+	$name = basename($srcfile);
+	$patchedfile = "$dir/patched_$name";
+}
 $patch = patcher_load($xexpatch);
 $patched = patcher_apply($xexfile, $patch);
 
+print_r($patch);
+
 if ($patched) {
 	file_put_contents($patchedfile, $patched);
-	echo "file patched as $patchedfile";
+	echo "file patched as $patchedfile\n\n";
 } else {
-	echo "file not patched";
+	echo "file not patched\n\n";
 }
 ?>
--- patcher/patcherlib.php
+++ patcher/patcherlib.php
@@ -1,15 +1,24 @@
 <?php 
 
+$last_search = null;
+
 function patcher_load_line($line) {
+	global $last_search;
+	
 	$parts = explode(" ", $line);
 
 	if (count($parts) < 3) return null;
-	if ($parts[0] != 'c') return null;
 	
-	$addr = hexdec($parts[1]);
+	$cmd = $parts[0];
+	if ($cmd != 'c' && $cmd!= 's' && $cmd!= 'r') return null;
+	
+	$index = 1;
+	if ($cmd == 'c') { // change has a base address
+		$addr = hexdec($parts[$index]);
+		$index++;
+	}
+	
 	$data = array();
-	
-	$index = 2;
 	do {
 		$part = $parts[$index];
 		if ($part == ";") break;
@@ -17,8 +26,43 @@ function patcher_load_line($line) {
 		
 		$data[] = hexdec($part);
 	} while (++$index < count($parts));
-	
-	return array('addr' => $addr, 'data' => $data);
+
+	if ($cmd == 'c') {
+		return array('cmd' => $cmd, 'addr' => $addr, 'data' => $data);
+	} if ($cmd == 's') {
+		$last_search = array ('cmd' => 's', 'search' => $data);
+		return null;
+	} if ($cmd == 'r' && $last_search != null) {
+		$result = $last_search;
+		$last_search = null;
+		
+		$result['replace'] = $data;
+		return $result;
+	}
+}
+
+function patcher_apply_bytes($addr_start, $addr_end, $bytes, $patches) {
+	foreach($patches as $patch) {
+		if ($patch['cmd'] == 'c') {
+			// implement when there is a patch of this type
+		} else if ($patch['cmd'] == 's') {
+			$search = $patch['search'];
+			$search_size = count($search);
+			
+			for($i=1; $i<=count($bytes); $i++) {
+				$search_index = 0;
+				while ($search_index < $search_size && $bytes[$i + $search_index] == $search[$search_index]) $search_index++;
+				if ($search_index == $search_size) {
+					print("block found\n");
+					$replace = $patch['replace'];
+					for($replace_index = 0; $replace_index < count($replace); $replace_index++) {
+						$bytes[$i + $replace_index] = $replace[$replace_index];
+					}
+				}
+			}
+		}
+	}
+	return $bytes;
 }
 
 function _read_word($handle) {
@@ -42,20 +86,27 @@ function patcher_apply($xexfile, $patch) {
 		$addr_start = _read_word($handle);
 		if ($addr_start < 0) break;
 		
-		$patched .= $addr_start;
+		$patched .= pack("S", $addr_start);
 		if ($addr_start == 0xFFFF) continue;
 		
 		$addr_end = _read_word($handle);
 		if ($addr_end < 0) break;
 		
-		$patched .= $addr_end;
+		$patched .= pack("S", $addr_end);
 		
 		$size = $addr_end - $addr_start + 1;
 		echo "block ".dechex($addr_start)." - ".dechex($addr_end)." - size $size\n";
 		$data = fread($handle, $size);
 		$bytes = unpack("C*", $data);
-	} while (!feof($handle));
+
+		$bytes = patcher_apply_bytes($addr_start, $addr_end, $bytes, $patch);
 		
+		foreach ($bytes as $byte) {
+			$patched .= pack("C", $byte);
+		}
+		
+	} while (!feof($handle));
+	return $patched;
 }
 
 function patcher_load($filename) {
