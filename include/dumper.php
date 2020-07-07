d00e0d2e23856b0cb4840cf3a16bf94722f63922
Franco Catrin <fcatrin@gmail.com>
2020-07-07 01:24:27 -0400

    refactor

--- include/dumper.php
+++ include/dumper.php
@@ -29,6 +29,37 @@ function dumper_read_blocks($blocks) {
 	return $result;
 }
 
+function dumper_xex_create() {
+	$xex = array();
+	$xex[] = 0xFF;
+	$xex[] = 0xFF;
+	return $xex;
+}
+
+function dumper_xex_add_header(&$xex, $addr_start, $addr_end) {
+	$xex[] = dumper_l($addr_start);
+	$xex[] = dumper_h($addr_start);
+	
+	$xex[] = dumper_l($addr_end);
+	$xex[] = dumper_h($addr_end);
+	return $xex;
+}
+
+function dumper_xex_add_data(&$xex, $data, $offset = 0) {
+	for($i=$offset; $i<count($data); $i++) {
+		$xex[] = $data[$i];
+	}
+}
+
+function dumper_pack($xex) {
+	$s = '';
+	foreach($xex as $x) {
+		$s .= pack("C", $x);
+	}
+	
+	return $s;
+}
+
 function dumper_boot_to_xex($boot, $eof) {
 	$boot_data = dumper_read_blocks($boot);
 	$boot_eof  = dumper_read_blocks($eof);
@@ -37,39 +68,17 @@ function dumper_boot_to_xex($boot, $eof) {
 	
 	$addr_end   = $addr_start + count($boot_data) - 6 - 1;
 	
-	$xex = array();
-	$xex[] = 0xFF;
-	$xex[] = 0xFF;
-	
-	$xex[] = dumper_l($addr_start);
-	$xex[] = dumper_h($addr_start);
-
-	$xex[] = dumper_l($addr_end);
-	$xex[] = dumper_h($addr_end);
-	
-	for($i=6; $i<count($boot_data); $i++) {
-		$xex[] = $boot_data[$i];
-	}
+	$xex = dumper_xex_create();
+	dumper_xex_add_header($xex, $addr_start, $addr_end);
+	dumper_xex_add_data($xex, $boot_data, 6);
 	
 	$addr_start = 0x400;
 	$addr_end   = $addr_start + count($boot_eof) - 1;
 
-	$xex[] = dumper_l($addr_start);
-	$xex[] = dumper_h($addr_start);
+	dumper_xex_add_header($xex, $addr_start, $addr_end);
+	dumper_xex_add_data($xex, $boot_eof);
 	
-	$xex[] = dumper_l($addr_end);
-	$xex[] = dumper_h($addr_end);
-
-	for($i=0; $i<count($boot_eof); $i++) {
-		$xex[] = $boot_eof[$i];
-	}
-	
-	$s = '';
-	foreach($xex as $x) {
-		$s .= pack("C", $x);
-	}
-	
-	return $s;
+	return dumper_pack($xex);
 }
 
 
