bffec97534b124975919e50462dd356794f6e9ef
Franco Catrin <fcatrin@gmail.com>
2020-07-07 01:24:40 -0400

    update boot block type

--- patches/koala/disasm/koala_boot_block_1.asm
+++ patches/koala/disasm/koala_boot_block_1.asm
@@ -3,115 +3,19 @@
 
                  org $0700
 
-                 rts
-                 jsr $2020
-                 .byte $62
-                 adc $2020,y
-                 .byte $52
-                 adc (TOADR+1,x)
-                 adc $6E61
-                 jsr $6F53
-                 ror ENDPT
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
-                 brk
+                 .byte $60, $20, $20, $20, $62, $79, $20, $20
+                 .byte $52, $61, $67, $6D, $61, $6E, $20, $53
+                 .byte $6F, $66, $74, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00, $00, $00, $00, $00, $00, $00
+                 .byte $00, $00
